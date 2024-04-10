30579f91afe6d278ac298e507f8a917f26902a20
EsterZanato <esterzanatocb@gmail.com>
2024-04-09 21:00:10 -0300

    alteração no código JS

--- js/script.js
+++ js/script.js
@@ -2,17 +2,15 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(function time() {
+function atualizarTempo() {
     let DateToday = new Date();
     let hr = DateToday.getHours();
     let min = DateToday.getMinutes();
-    let s = DateToday.getSeconds ();
+    let s = DateToday.getSeconds();
 
     horas.textContent = hr.toString().padStart(2, '0');
     minutos.textContent = min.toString().padStart(2, '0')
     segundos.textContent = s.toString().padStart(2, '0');
+}
 
-    horas.textContent = hr;
-    minutos.textContent = min;
-    segundos.textContent = s;
-})
+setInterval(atualizarTempo)
